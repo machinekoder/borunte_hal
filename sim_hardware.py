89f7d2047f1e511846d513d1afbd9669e4081072
Alexander Rössler <mail@roessler.systems>
2018-11-25 13:45:32 +0000

    add more complete stepgen to sim config

--- sim_hardware.py
+++ sim_hardware.py
@@ -1,10 +1,12 @@
 # coding=utf-8
 from machinekit import hal
 from machinekit import rtapi as rt
+from math import pi
 
-from utils import PinGroup
+import yaml
 
 NUM_JOINTS = 6
+JOINT_CONFIG = 'joint_config.yml'
 
 
 class Hardware(object):
@@ -17,27 +19,57 @@ class Hardware(object):
         self._init_encoders()
 
     def read(self):
-        hal.addf('stepgen.capture-position', self.thread.name)
+        for i in range(NUM_JOINTS):
+            hal.addf('stepgen-{}.read'.format(i), self.thread.name)
         hal.addf('encoder.capture-position', self.thread.name)
         hal.addf('encoder.update-counters', self.thread.name)
 
     def setup(self):
+        self._setup_joints()
         self._setup_brakes()
-        self._setup_stepgens()
         self._setup_estop()
 
     def write(self):
-        hal.addf('stepgen.make-pulses', self.thread.name)
-        hal.addf('stepgen.update-freq', self.thread.name)
+        for i in range(NUM_JOINTS):
+            hal.addf('stepgen-{}.write'.format(i), self.thread.name)
+
+    def _setup_joints(self):
+        with open(JOINT_CONFIG, 'r') as f:
+            config = yaml.safe_load(f)
+
+        for i in range(1, NUM_JOINTS + 1):
+            c = config['joint_{}'.format(i)]
+            scale = c['gear_ratio'] * c['steps_per_rev'] / (2.0 * pi)
+            nr = 6 - i
+
+            # stepgen
+            stepgen = hal.instances['stepgen-{}'.format(nr)]
+            stepgen.pin('position-scale').set(scale)
+            stepgen.pin('maxvel').set(c['max_vel_rad_s'] / 10)
+            stepgen.pin('maxaccel').set(c['max_accel_rad_s2'] / 10)
+            stepgen.pin('enable').link('son-{}-out'.format(i))
+            stepgen.pin('position-cmd').link('joint-{}-cmd-out-pos'.format(i))
+            stepgen.pin('position-fb').link('joint-{}-cmd-fb-pos'.format(i))
+
+            # encoder
+            sum2 = rt.newinst('sum2v2', 'sum2-encoder-{}-fb'.format(nr))
+            hal.addf(sum2.name, self.thread.name)
+            sum2.pin('in0').link('joint-{}-cmd-fb-pos'.format(i))
+            sum2.pin('in1').set(0.0)  # can be used to set an artificial offset to trigger the alarm
+            sum2.pin('out').link('joint-{}-fb-in-pos'.format(i))
+
+            # encoder abs
+            hal.Signal('joint-{}-abs-pos'.format(i)).set(c['home_pos'])
+            hal.Signal('joint-{}-home-pos'.format(i)).set(c['home_pos'])
+
+            # setup limits
+            hal.Signal('joint-{}-limit-min'.format(i)).set(c['min_limit_rad'])
+            hal.Signal('joint-{}-limit-max'.format(i)).set(c['max_limit_rad'])
+            hal.Signal('joint-{}-ferror-max'.format(i)).set(c['max_ferror_rad'])
 
     def _init_stepgens(self):
-        rt.loadrt('stepgen', step_type=','.join(['0'] * NUM_JOINTS))
-
-    def _setup_stepgens(self):
-        for i in range(1, NUM_JOINTS + 1):
-            nr = 6 - i
-            stepgen = PinGroup('stepgen.{}'.format(nr))
-            stepgen.pin('enable').link('son-{}-out'.format(i))
+        for i in range(NUM_JOINTS):
+            rt.newinst('dummy_stepgen', 'stepgen-{}'.format(i))
 
     def _init_encoders(self):
         rt.loadrt('encoder', num_chan=NUM_JOINTS)
